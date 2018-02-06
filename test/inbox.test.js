ddb157cd00be7eca30107cdfaa7fc177f05156d3
liebeskind <daniel.liebeskind@gmail.com>
2018-02-05 17:08:06 -0800

    Get ganache test eth accounts

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,28 +3,45 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return 'stopped';
-  }
-
-  drive() {
-    return 'vroom';
-  }
-}
-
-let car;
-
 beforeEach(() => {
-  car = new Car();
+  // Get a list of all accounts
+  web3.eth.getAccounts().then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
+
+  // Use one of those accounts to deploy the contract
+
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+
+  });
 })
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
-  });
 
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  });
-});
+// class Car {
+//   park() {
+//     return 'stopped';
+//   }
+
+//   drive() {
+//     return 'vroom';
+//   }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//   car = new Car();
+// })
+
+// describe('Car', () => {
+//   it('can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   });
+
+//   it('can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   });
+// });
