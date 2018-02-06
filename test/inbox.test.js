d2577042a6b00b5652de4532cec50c8d6712c6fb
liebeskind <daniel.liebeskind@gmail.com>
2018-02-05 18:24:45 -0800

    Fixes bug in this version of web3

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -15,6 +18,8 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface)) //Send ABI into web3 contract function.
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' }) //Account we want to deploy from
+
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
