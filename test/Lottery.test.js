dfb8d89cf65cc09dd775e7246a2b8263432a7137
liebeskind <daniel.liebeskind@gmail.com>
2018-02-07 14:23:39 -0800

    Both tests passing

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
@@ -14,6 +15,8 @@ beforeEach(async () => {
   lottery = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
+
+    lottery.setProvider(provider);
 });
 
 describe('Lottery Contract', () => {
@@ -27,7 +30,12 @@ describe('Lottery Contract', () => {
       value: web3.utils.toWei('0.02', 'ether') //prevents us from having to add all zeros of wei
     })
 
-    const players = await lottery.methods.getPlayers().call();
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
   })
 
 });
