b5cd9375255c777cdcf98026023b107db2d8ad8a
liebeskind <daniel.liebeskind@gmail.com>
2018-02-06 18:28:23 -0800

    Small fixes to customize for 'lottery'

--- deploy.js
+++ deploy.js
@@ -18,7 +18,7 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data:bytecode, arguments: [text] })
+    .deploy({ data:bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to ', result.options.address);
